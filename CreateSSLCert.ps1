d39662de396fcc81a79325d675fba00e0c48b1fa
Paul Hoffman <pvhoffman@gmail.com>
2016-09-09 13:18:13 -0700

    changed hash to sha256

--- CreateSSLCert.ps1
+++ CreateSSLCert.ps1
@@ -2,5 +2,5 @@ param(
         [string]$Name
 )
 
-makecert -iv DevRoot.pvk -ic DevRoot.cer -n "CN=$Name" -pe -sv "$Name.pvk" -a sha1 -len 2048 -b 01/21/2010 -e 01/21/2020 -sky exchange "$Name.cer" -eku 1.3.6.1.5.5.7.3.1
+makecert -iv DevRoot.pvk -ic DevRoot.cer -n "CN=$Name" -pe -sv "$Name.pvk" -a sha256 -len 2048 -b 01/21/2010 -e 01/21/2020 -sky exchange "$Name.cer" -eku 1.3.6.1.5.5.7.3.1
 pvk2pfx -pvk "$Name.pvk" -spc "$Name.cer" -pfx "$Name.pfx"
